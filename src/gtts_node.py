094ac18210da9fd699a145cf656d939c128ce1e9
Hazeeq02 <92501590+Hazeeq02@users.noreply.github.com>
2024-06-24 16:43:10 +0800

    Create gtts_node.py

---
+++ src/gtts_node.py
@@ -0,0 +1,35 @@
+# install dulu nie sebelum run: 
+# pip install gtts
+# sudo apt-get install mpg123
+
+# klu x de pip, install nie dulu:
+# sudo apt-get update
+# sudo apt-get install python-pip
+
+#!/usr/bin/env python3
+
+import rospy
+from gtts import gTTS
+import os
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(f"Received text: {data.data}")
+    mytext = data.data
+    language = 'en'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save("/tmp/welcome.mp3")
+    os.system("mpg123 /tmp/welcome.mp3")
+
+def listener():
+    rospy.init_node('gtts_node', anonymous=True)
+    rospy.Subscriber('gtts_text', String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
+  
+# lps install, pergi dkt package.xml and tmbh 2 dependency nie
+# <build_depend>gtts</build_depend>
+# <exec_depend>gtts</exec_depend>
