e81ec7f4e1a1121934d713418dc91d45bdb6262d
AfdaPrime <afiq.danish306@gmail.com>
2024-06-25 14:39:34 +0800

    ros workspace

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/vboxuser/ws_catkin/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/vboxuser/ws_catkin/devel;/opt/ros/noetic'.split(';') if '/home/vboxuser/ws_catkin/devel;/opt/ros/noetic' != '' else []
---
+++ build/face_recognition/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vboxuser/ws_catkin/src/face_recognition
+# Build directory: /home/vboxuser/ws_catkin/build/face_recognition
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
